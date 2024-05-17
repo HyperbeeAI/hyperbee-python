8a8f2d0e80a55e1645b26eb8c340d52f017987c8
cemalgndzz <cgogit@gmail.com>
2024-05-17 13:54:25 +0000

    multiple base urls implemented

--- src/hyperbee/resources/batch_request/batch_request.py
+++ src/hyperbee/resources/batch_request/batch_request.py
@@ -11,23 +11,35 @@ class batch_request():
     
     def __init__(self, api_key):
         self.base_url = "http://34.123.162.171:30002"
+        self.base_url2 = "http://35.202.29.116:30001"
         self.client = httpx.Client(timeout=180.0)
-        self.thread_cnt = 150
+        self.client2 = httpx.Client(timeout=180.0)
+        self.thread_cnt = 160
+        self.thread_cnt2 = 60
         
     def __call__(self, prompt_list: List[str], output_length: int):
         self.base_url = "http://34.123.162.171:30002"
+        self.base_url2 = "http://35.202.29.116:30001"
         self.client = httpx.Client(timeout=180.0)
+        self.client2 = httpx.Client(timeout=180.0)
         output_length = output_length + 1
         threads = []
         result_queue = queue.Queue()
-        prompt_per_thread = (len(prompt_list) // self.thread_cnt) + 1
-
+        
+        total_thread_cnt = self.thread_cnt + self.thread_cnt2
+        prompt_per_thread = (len(prompt_list) // total_thread_cnt) + 1
+        print(f"Per thread prompt length : {prompt_per_thread}")
         try:
+            thread_index = 1
             for i in range(0, len(prompt_list), prompt_per_thread):
                 batch = prompt_list[i:i + prompt_per_thread]
                 batch_tuples = [(f"<|user|>\n{prompt}<|end|>\n<|assistant|>", output_length) for prompt in batch]
-                t = threading.Thread(target=send_batch, args=(self.client, self.base_url, batch_tuples, i+1, result_queue))
+                if thread_index <= self.thread_cnt:
+                    t = threading.Thread(target=send_batch, args=(self.client, self.base_url, batch_tuples, thread_index, result_queue))
+                else:
+                    t = threading.Thread(target=send_batch, args=(self.client2, self.base_url2, batch_tuples, thread_index, result_queue))
                 threads.append(t)
+                thread_index += 1
                 t.start()
                 
             for t in threads:
@@ -49,8 +61,11 @@ def extract_required_part(output: str) -> str:
     return output.split("<|end|>")[0][1:]
 
 def send_batch(client, base_url, requests, thread_id, result_queue):
+    start = time.time()
     results = run_vllm(client, base_url, requests)
     result_queue.put((thread_id, results))
+    end = time.time()
+    print(f"Thread {thread_id} finished in {end-start:.2f}s")
 
 def run_vllm(client, base_url, requests: List[Tuple[str, int]]) -> List[Tuple[str, str]]:
     request_items = [{'prompt': prompt, 'output_len': output_len} for prompt, output_len in requests]
@@ -60,6 +75,4 @@ def run_vllm(client, base_url, requests: List[Tuple[str, int]]) -> List[Tuple[st
     batch_results = [item['result'] for item in results]
     processed_list = [extract_required_part(string) for string in batch_results]
     return processed_list
-    
-    
     
