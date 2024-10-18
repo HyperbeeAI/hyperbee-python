7c63de19d8fa3f635d1759b91c472d3c1c9918a1
unknown <erensenogl@gmail.com>
2024-10-18 18:00:36 +0200

    Update: documentation

--- src/hyperbee/resources/chat/completions.py
+++ src/hyperbee/resources/chat/completions.py
@@ -402,10 +402,8 @@ class Completions(SyncAPIResource):
     ### Fetches the list of documents readily available in a remote namespace
     ### Errors out if the namespace ID is None
     ### Errors out if the returned doc list is empty since that means either the namespace does not exist, or that it only has non-indexed files (limbo state)
-    ### Errors out if the namespace ID is None
-    ### Errors out if the returned doc list is empty since that means either the namespace does not exist, or that it only has non-indexed files (limbo state)
     ### Errors out if the API returns something erroneous (other than 200)
-    def __get_remote_doclist(self, namespace: str) -> List[str]:
+    def get_remote_doclist(self, namespace: str) -> List[str]:
 
         with httpx.Client() as client:
             response = client.post(
@@ -457,7 +455,7 @@ class Completions(SyncAPIResource):
         timeout_flag = False
         start_time = time.time()
         while not stop_trying:
-            remote_doclist = self.__get_remote_doclist(namespace=namespace)
+            remote_doclist = self.get_remote_doclist(namespace=namespace)
             if verbose:
                 print("Remote: ", remote_doclist)
                 print("Local:  ", local_doclist)
@@ -611,7 +609,7 @@ class Completions(SyncAPIResource):
         Raises:
             ValueError: If namespace is None.
         """
-        remote_doclist = self.__get_remote_doclist(namespace=namespace)
+        remote_doclist = self.get_remote_doclist(namespace=namespace)
 
         # Extract filenames from uploadlist
         uploadlist_filenames = [os.path.basename(file) for file in uploadlist]
@@ -700,7 +698,7 @@ class Completions(SyncAPIResource):
         Raises:
             ValueError: If namespace is None.
         """
-        remote_doclist = self.__get_remote_doclist(namespace=namespace)
+        remote_doclist = self.get_remote_doclist(namespace=namespace)
 
         deletelist_filenames = [os.path.basename(file) for file in deletionlist]
 
@@ -768,7 +766,7 @@ class Completions(SyncAPIResource):
         Raises:
             ValueError: If namespace is None.
         """
-        remote_doclist = self.__get_remote_doclist(namespace=namespace)
+        remote_doclist = self.get_remote_doclist(namespace=namespace)
         local_doclist_filenames = self.__get_local_doclist(folder_path=folder_path)
         local_doclist = [os.path.join(folder_path, file) for file in local_doclist_filenames]
 
